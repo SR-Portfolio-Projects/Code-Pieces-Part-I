51815423e4f6b04bd2316de117cd33f55a6018b2
shareed <shanreed6@gmail.com>
2020-09-30 16:18:54 -0700

    comments now show when the comment button is clicked

--- code-pieces/src/components/Comments.js
+++ code-pieces/src/components/Comments.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faComments } from '@fortawesome/free-solid-svg-icons'
+import { faComments, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 
 import Comment from './comments/Comment';
@@ -8,23 +8,46 @@ import Comment from './comments/Comment';
 
 
 const Comments = (props) => {
-    const { numOfComments, commentsList } = props;
+	const { numOfComments, commentsList } = props;
+	const [commentsOpen, setCommentsOpen] = useState(false)
+	console.log(commentsList.length);
+
+	const handleClick = () => {
+		console.log('clicked');
+		setCommentsOpen({commentsOpen: !commentsOpen})
+		console.log(commentsOpen);
+		
+	}
 
 	return (
-	    <div className="comment-section" key="comment-icons-container">
-			<div className="comments-section-wrapper">
-				<p className="comments-number">{ numOfComments } comments</p>
-					<FontAwesomeIcon icon = { faComments } />
+	    <div className="comment-section-container" key="comment-container">
+
+			<div className = 'comments-button' onClick = { handleClick }>
+						<p className="comments-number">{ numOfComments } comments</p>
+						<FontAwesomeIcon icon = { faComments } />
 			</div>
-                { commentsList ?
-                    commentsList.map((comment, index) => {
-                        return <Comment key = {index} comment = { comment } />
-                    
-                }) : null}
+
+				{ commentsList.length && (
+
+					<div className = {`comments ${commentsOpen ? 'show' : ''}`}>
+
+						{commentsList.map((comment, index) => (
+
+                            <div className = 'comment-list-item' key = {index} >
+								<Comment comment = { comment } />
+                            </div>
+
+                        ))}
+
+						<div className = 'close-comments-button' onClick = { handleClick }>
+							<FontAwesomeIcon icon = { faTimes } />
+                  		</div>
+
+					</div>
+				)}
                 
 		</div>
 	);
-}
-;
+};
 
 export default Comments;
